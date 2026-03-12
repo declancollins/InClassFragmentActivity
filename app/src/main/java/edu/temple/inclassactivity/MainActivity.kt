4facfe1ff8ea8031afe130def54e175784c87111
declancollins <declancollins05@gmail.com>
2026-03-12 11:52:48 -0400

    showed recycler view through fragment

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         typedArray.recycle()
 
         // Attach an instance of ImageDisplayFragment using factory method
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView,ImageDisplayFragment.newInstance(imageArray)).commit()
 
     }
 }
